d256acc1f198085ef4ef61a28dd1aa083737012a
gpc2602 <119427605+gpc2602@users.noreply.github.com>
2025-04-27 17:16:49 -0500

    Delete src/main/java/com/visiontech/repositories/RutaRepository.java

--- src/main/java/com/visiontech/repositories/RutaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.visiontech.repositories;
-
-import com.visiontech.entities.Ruta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface RutaRepository extends JpaRepository<Ruta, Integer> {
-}
