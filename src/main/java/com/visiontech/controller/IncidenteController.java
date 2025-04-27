88ecee083070b5292248161fdde8c6482971e022
gpc2602 <119427605+gpc2602@users.noreply.github.com>
2025-04-27 17:13:01 -0500

    Update IncidenteController.java

--- src/main/java/com/visiontech/controller/IncidenteController.java
+++ src/main/java/com/visiontech/controller/IncidenteController.java
@@ -1,31 +1,63 @@
 package com.visiontech.controller;
 
 
-import com.visiontech.dto.DTOIncident;
-import com.visiontech.entities.Incidente;
-import com.visiontech.services.ServiceRoute;
+import com.visiontech.dto.IncidenteDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.*;
+import com.visiontech.entities.Incidente;
+import com.visiontech.services.IncidenteService;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/incidentes")
 public class IncidenteController {
     @Autowired
-    private IncidenteController incidenteController;
+    private IncidenteService iR;
 
-    @GetMapping("/incidentes")
-    public List<DTOIncident> list() {
-        return incidenteController.list().stream().map(Incidente x ->{
+    @GetMapping
+    public List<IncidenteDTO> listar() {
+        return iR.list().stream().map(x -> {
             ModelMapper m = new ModelMapper();
-            return m.map(x, DTOIncident.class);
+            return m.map(x, IncidenteDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public void registrar(@RequestBody IncidenteDTO iDTO) {
+        ModelMapper m = new ModelMapper();
+        Incidente i = m.map(iDTO, Incidente.class);
+        iR.insert(i);
+    }
+
+    @GetMapping("/{idIncidente}")
+    public IncidenteDTO listarId(@PathVariable("idIncidente") int idIncidente) {
+        ModelMapper m = new ModelMapper();
+        IncidenteDTO dto = m.map(iR.listId(idIncidente), IncidenteDTO.class);
+        return dto;
+    }
+
+    @PutMapping
+    public void modificar(@RequestBody IncidenteDTO iDTO) {
+        ModelMapper m = new ModelMapper();
+        Incidente i = m.map(iDTO, Incidente.class);
+        iR.update(i);
+    }
+
+    @DeleteMapping("/{idIncidente}")
+    public void eliminar(@PathVariable("idIncidente") int idIncidente) {
+        iR.delete(idIncidente);
+    }
+
+    @GetMapping("/busquedas")
+    public List<IncidenteDTO> buscar(@RequestParam String t) {
+        return iR.buscarPorTipo(t).stream().map(x -> {
+            ModelMapper m = new ModelMapper();
+            return m.map(x, IncidenteDTO.class);
         }).collect(Collectors.toList());
     }
 
 }
+
