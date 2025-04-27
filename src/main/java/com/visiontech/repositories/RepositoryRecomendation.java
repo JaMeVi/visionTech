aaa4b4defd9f953b5d67ca1b50116aff19a1f204
gpc2602 <119427605+gpc2602@users.noreply.github.com>
2025-04-27 17:16:37 -0500

    Delete src/main/java/com/visiontech/repositories/RepositoryRecomendation.java

--- src/main/java/com/visiontech/repositories/RepositoryRecomendation.java
+++
@@ -1,7 +0,0 @@
-package com.visiontech.repositories;
-
-import com.visiontech.entities.Recomendacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepositoryRecomendation extends JpaRepository<Recomendacion, Integer> {
-}
