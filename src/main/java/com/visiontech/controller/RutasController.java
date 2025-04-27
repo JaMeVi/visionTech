8de8b04c6590e8490c4754e9815367c7fe3814ab
gpc2602 <119427605+gpc2602@users.noreply.github.com>
2025-04-26 22:26:07 -0500

    Update RutasController.java

--- src/main/java/com/visiontech/controller/RutasController.java
+++ src/main/java/com/visiontech/controller/RutasController.java
@@ -1,4 +1,38 @@
 package com.visiontech.controller;
 
+import com.visiontech.dto.DTORoute;
+import com.visiontech.entities.Ruta;
+import com.visiontech.services.ServiceRoute;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api")
 public class RutasController {
+    @Autowired
+    ServiceRoute serviceRoute;
+
+    @GetMapping("/rutas")
+    public List<DTORoute> getAll(){
+        return serviceRoute.findAll();
+    }
+    @GetMapping("/rutas/{id}")
+    public DTORoute getById(@PathVariable int id){
+        return serviceRoute.findById(id);
+    }
+    @PostMapping("/rutas")
+    public DTORoute create(@RequestBody DTORoute dTORoute){
+        return serviceRoute.save(dTORoute);
+    }
+    @PutMapping("/rutas/{id}")
+    public DTORoute update(@PathVariable int id, @RequestBody DTORoute dTORoute){
+        return serviceRoute.update(id, dTORoute);
+    }
+    @DeleteMapping("/rutas/{id}")
+    public boolean delete(@PathVariable int id){
+        return serviceRoute.delete(id);
+    }
+
 }
