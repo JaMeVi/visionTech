d85d704bf85505166519d2340b0b171b46f177f4
gpc2602 <119427605+gpc2602@users.noreply.github.com>
2025-04-27 17:16:04 -0500

    Delete src/main/java/com/visiontech/repositories/RepositoryIncident.java

--- src/main/java/com/visiontech/repositories/RepositoryIncident.java
+++
@@ -1,7 +0,0 @@
-package com.visiontech.repositories;
-
-import com.visiontech.entities.Ruta;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepositoryIncident extends JpaRepository<Ruta, Integer> {
-}
