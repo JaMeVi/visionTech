061c877143b9ed6f4f2ac88e86f6453e771a55ed
Jan Mendoza V <u201924912@upc.edu.pe>
2025-04-28 23:37:52 -0500

    Controller Metricas

--- src/main/java/com/visiontech/controller/MetricaController.java
+++ src/main/java/com/visiontech/controller/MetricaController.java
@@ -1,8 +1,14 @@
 package com.visiontech.controller;
 
+import com.visiontech.dto.MetricasDTO;
+import com.visiontech.entities.Metrica;
 import com.visiontech.services.MetricaService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/metricas")
@@ -10,23 +16,30 @@ public class MetricaController{
 
     @Autowired
     private MetricaService mS;
+
     @GetMapping
-    public String listar(){
-        return "Metricas";
+    public List<MetricasDTO> listar(){
+        return mS.list().stream().map(x->{
+            ModelMapper modelMapper = new ModelMapper();
+            return modelMapper.map(x, MetricasDTO.class) ;
+    }).collect(Collectors.toList()) ;
     }
 
     @PostMapping
-    public void insertar(){}
-
-    @GetMapping("/{idMetrica}")
-    public String listarId(){
-        return "Metricas";
+    public void insertar(@RequestBody MetricasDTO mDto){
+    ModelMapper modelMapper = new ModelMapper();
+        Metrica m = modelMapper.map(mDto, Metrica.class);
+        mS.insert(m);
     }
 
     @PutMapping
-    public void modificar(){}
+    public void modificar(@RequestBody MetricasDTO mDto){
+    ModelMapper modelMapper = new ModelMapper();
+    Metrica m = modelMapper.map(mDto, Metrica.class);
+    mS.update(m);
+    }
 
     @DeleteMapping("/{idMetrica}")
-    public void eliminar(){}
+    public void eliminar(@PathVariable("idMetrica") int idMetrica){mS.delete(idMetrica);}
 
 }
