c73e2037880e32e98f127b5737ef51fab518a748
AdrianoZombie18 <112348002+AdrianoZombie18@users.noreply.github.com>
2025-04-29 00:42:56 -0500

    Update ContactoEmergenciaController.java

--- src/main/java/com/visiontech/controller/ContactoEmergenciaController.java
+++ src/main/java/com/visiontech/controller/ContactoEmergenciaController.java
@@ -1,20 +1,54 @@
 package com.visiontech.controller;
 
+import com.visiontech.dto.ContactoEmergenciaDTO;
 import com.visiontech.entities.Contacto_emergencia;
 import com.visiontech.services.ContactoEmergenciaService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/contactos-emergencia")
+@RequestMapping("/contactoemergencia")
 public class ContactoEmergenciaController {
 
     @Autowired
     private ContactoEmergenciaService contactoEmergenciaService;
 
-    @GetMapping("/busquedas")
-    public List<Contacto_emergencia> buscarContactosPorNombre(@RequestParam String nombre) {
-        return contactoEmergenciaService.buscarPorNombre(nombre);
+    @GetMapping
+    public List<ContactoEmergenciaDTO> listar() {
+        return contactoEmergenciaService.list().stream().map(contacto -> {
+                    ModelMapper modelMapper = new ModelMapper();
+                    return modelMapper.map(contacto, ContactoEmergenciaDTO.class);
+                })
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping
+    public void insert(@RequestBody ContactoEmergenciaDTO contactoEmergenciaDTO) {
+        ModelMapper modelMapper = new ModelMapper();
+        Contacto_emergencia contacto = modelMapper.map(contactoEmergenciaDTO, Contacto_emergencia.class);
+        contactoEmergenciaService.insert(contacto);
+    }
+
+    @GetMapping("/{idContactoEmergencia}")
+    public ContactoEmergenciaDTO listId(@PathVariable("idContactoEmergencia") int idContactoEmergencia) {
+        ModelMapper modelMapper = new ModelMapper();
+        ContactoEmergenciaDTO dto = modelMapper.map(contactoEmergenciaService.listId(idContactoEmergencia), ContactoEmergenciaDTO.class);
+        return dto;
+    }
+
+    @PutMapping
+    public void update(@RequestBody ContactoEmergenciaDTO contactoEmergenciaDTO) {
+        ModelMapper modelMapper = new ModelMapper();
+        Contacto_emergencia contacto = modelMapper.map(contactoEmergenciaDTO, Contacto_emergencia.class);
+        contactoEmergenciaService.update(contacto);
+    }
+
+    @DeleteMapping("/{idContactoEmergencia}")
+    public void delete(@PathVariable("idContactoEmergencia") int idContactoEmergencia) {
+        contactoEmergenciaService.delete(idContactoEmergencia);
     }
 }
