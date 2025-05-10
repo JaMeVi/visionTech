0cd11540bb1d030f5d8d2189595e1a524a4f2d7e
gpc2602 <119427605+gpc2602@users.noreply.github.com>
2025-05-10 10:09:03 -0500

    Update RutaDTO.java

--- src/main/java/com/visiontech/dto/RutaDTO.java
+++ src/main/java/com/visiontech/dto/RutaDTO.java
@@ -12,12 +12,104 @@ public class RutaDTO {
     private String nombreRuta;
     private String destino;
     private String inicio;
-    private int distanciaMinutos;
+    private String fin;
+    private boolean favorito;
+    private int distanciaMetros;
     private int tiempoRuta;
     private int longitud;
     private int latitud;
-    private int idUsuario;
+    private Usuario usuario;
+
+    public int getIdRuta() {
+        return idRuta;
+    }
+
+    public void setIdRuta(int idRuta) {
+        this.idRuta = idRuta;
+    }
+
+    public String getNombreRuta() {
+        return nombreRuta;
+    }
+
+    public void setNombreRuta(String nombreRuta) {
+        this.nombreRuta = nombreRuta;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public String getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(String inicio) {
+        this.inicio = inicio;
+    }
+
+    public String getFin() {
+        return fin;
+    }
+
+    public void setFin(String fin) {
+        this.fin = fin;
+    }
+
+    public boolean isFavorito() {
+        return favorito;
+    }
+
+    public void setFavorito(boolean favorito) {
+        this.favorito = favorito;
+    }
+
+    public int getDistanciaMetros() {
+        return distanciaMetros;
+    }
+
+    public void setDistanciaMetros(int distanciaMetross) {
+        this.distanciaMetros = distanciaMetross;
+    }
+
+    public int getTiempoRuta() {
+        return tiempoRuta;
+    }
+
+    public void setTiempoRuta(int tiempoRuta) {
+        this.tiempoRuta = tiempoRuta;
+    }
+
+    public int getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(int longitud) {
+        this.longitud = longitud;
+    }
+
+    public int getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(int latitud) {
+        this.latitud = latitud;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
+
+
 
 
 
